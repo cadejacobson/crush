7ab93766e2f5ff64de74b9cc350f824cfd32c52c
Cade Jacobson <cade.jacobson@mines.sdsmt.edu>
2024-03-01 18:18:37 -0700

    Create basic input checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("crush: > ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read input");
+
+        let user_input = user_input.trim();
+
+        if user_input == "exit" {
+            break;
+        }
+
+        if user_input == ""{
+            continue;
+        }
+        println!("You entered: {}", user_input);
+    }
 }
