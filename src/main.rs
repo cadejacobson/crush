3c80c809619cc08f71a87d923becac6655fdb672
Cade Jacobson <91996442+cadejacobson@users.noreply.github.com>
2024-03-03 14:18:44 -0700

    Create file output (#8)
    * create a way to parse the string
    
    * Add file output implementation
    
    * formatting
    
    * Finish fileoutput
    
    * Remove extra files
    
    ---------
    
    Co-authored-by: Cade Jacobson <cade.jacobson@mines.sdsmt.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::fs::File;
 use std::io;
 use std::io::Write;
 use std::process::{Command, Stdio};
+
+struct SingleCommand {
+    tokens: Vec<String>,
+    output_filename: Option<String>,
+    directed_output: bool,
+}
 
 fn main() -> io::Result<()> {
     loop {
@@ -8,7 +15,9 @@ fn main() -> io::Result<()> {
         io::stdout().flush().expect("Failed to flush stdout");
 
         let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input).expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Failed to read input");
 
         let user_input = user_input.trim();
 
@@ -16,32 +25,83 @@ fn main() -> io::Result<()> {
             return Ok(());
         }
 
-        if user_input == ""{
+        if user_input == "" {
             continue;
         }
 
-        let mut tokens: Vec<&str> = user_input.split_whitespace().collect();
-        let exe = tokens[0];
-        tokens.remove(0);
+        let tokens: Vec<&str> = user_input.split_whitespace().collect();
+        let single_command = parse_user_input(tokens.clone());
 
-        let mut handles = vec![];
+        let _ = execute_command(single_command);
+    }
+}
 
-        let command = Command::new(exe).args(tokens)
-            .stdout(Stdio::inherit())
-            .spawn();      
+fn parse_user_input(tokens: Vec<&str>) -> SingleCommand {
+    let mut command = SingleCommand {
+        tokens: vec![],
+        output_filename: None,
+        directed_output: false,
+    };
 
-            match command {
-                Ok(handle) => {
-                    handles.push(handle); // Store the handle
-                },
-                Err(err) => {
-                    eprintln!("Failed to execute '{}': {}", exe, err);
-                    continue;
-                }
-            };
-                    // Wait for all processes to finish
-        for mut handle in handles {
-            handle.wait()?;
+    let len = tokens.len();
+    let mut i = 0;
+
+    while i < len {
+        if tokens[i] == ">" {
+            command.directed_output = true;
+            if (i + 1) < len && !is_operator(tokens[i + 1]) {
+                command.output_filename = Some(tokens[i + 1].to_owned());
+                i += 1;
+            }
+        } else {
+            command.tokens.push(tokens[i].to_owned());
+        }
+        i += 1;
+    }
+
+    return command;
+}
+
+fn is_operator(token: &str) -> bool {
+    if token == ">" {
+        return true;
+    }
+
+    return false;
+}
+
+fn execute_command(command: SingleCommand) {
+    let mut handles = vec![];
+
+    let stdout_handle: Stdio = if command.directed_output {
+        let file_path = command.output_filename.unwrap();
+        let file = File::create(&file_path).unwrap();
+        Stdio::from(file)
+    } else {
+        Stdio::inherit()
+    };
+
+    let result = Command::new(command.tokens[0].as_str())
+        .args(&command.tokens[1..])
+        .stdout(Stdio::from(stdout_handle))
+        .spawn();
+
+    match result {
+        Ok(handle) => {
+            handles.push(handle);
+        }
+        Err(err) => {
+            eprintln!(
+                "Failed to execute '{}': {}",
+                command.tokens[0].as_str(),
+                err
+            );
+        }
+    };
+
+    for mut handle in handles {
+        if let Err(err) = handle.wait() {
+            eprintln!("Failed to wait for command to finish: {}", err);
         }
     }
 }
