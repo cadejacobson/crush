342f435d61b941112ce6b6e880ce39fe185935d5
Cade Jacobson <91996442+cadejacobson@users.noreply.github.com>
2024-04-27 19:39:04 -0600

    Setup piping input and output (#17)
    * Lay the groundwork for piping
    
    * Add working input and output handling
    
    * Reformat code
    
    ---------
    
    Co-authored-by: Cade Jacobson <cade.jacobson@mines.sdsmt.edu>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::fs::File;
 use std::io::Write;
-use std::process::{Command, Stdio};
+use std::process::{Child, Command, Stdio};
 use std::{env, io};
 
+#[derive(Clone)]
 struct SingleCommand {
     tokens: Vec<String>,
     output_filename: Option<String>,
     input_filename: Option<String>,
-    directed_output: bool,
-    directed_input: bool,
+    piped_input: bool,     /* For the pipe operator | */
+    piped_output: bool,    /* For the pipe operator | */
+    directed_output: bool, /* For the file operator > */
+    directed_input: bool,  /* For the file operator > */
 }
 
 fn main() -> io::Result<()> {
@@ -39,96 +42,125 @@ fn main() -> io::Result<()> {
         }
 
         let tokens: Vec<&str> = user_input.split_whitespace().collect();
-        let command = parse_user_input(tokens.clone());
+        let commands = parse_user_input(tokens.clone());
 
-        if command.tokens[0] == "cd" {
-            change_dir(command);
+        if commands[0].tokens[0] == "cd" {
+            change_dir(commands[0].clone());
             continue;
         }
 
-        let _ = execute_command(command);
+        let _ = execute_commands(commands);
     }
 }
 
-fn parse_user_input(tokens: Vec<&str>) -> SingleCommand {
-    let mut command = SingleCommand {
-        tokens: vec![],
-        output_filename: None,
-        input_filename: None,
-        directed_output: false,
-        directed_input: false,
-    };
+fn parse_user_input(tokens: Vec<&str>) -> Vec<SingleCommand> {
+    let mut commands = Vec::new();
 
     let len = tokens.len();
     let mut i = 0;
+    let mut pipe_next_input = false;
 
     while i < len {
-        if tokens[i] == ">" {
-            command.directed_output = true;
-            if (i + 1) < len && !is_operator(tokens[i + 1]) {
-                command.output_filename = Some(tokens[i + 1].to_owned());
-                i += 1;
-            }
-        } else if tokens[i] == "<" {
-            command.directed_input = true;
-            if (i + 1) < len && !is_operator(tokens[i + 1]) {
-                command.input_filename = Some(tokens[i + 1].to_owned());
-                i += 1;
-            }
-        } else {
-            command.tokens.push(tokens[i].to_owned());
+        let mut command = SingleCommand {
+            tokens: vec![],
+            output_filename: None,
+            input_filename: None,
+            piped_input: false,
+            piped_output: false,
+            directed_output: false,
+            directed_input: false,
+        };
+
+        if pipe_next_input {
+            command.piped_input = true;
         }
-        i += 1;
+
+        pipe_next_input = false;
+
+        while i < len && tokens[i] != "|" {
+            if tokens[i] == ">" {
+                command.directed_output = true;
+                if (i + 1) < len && !is_operator(tokens[i + 1]) {
+                    command.output_filename = Some(tokens[i + 1].to_owned());
+                    i += 1;
+                }
+            } else if tokens[i] == "<" {
+                command.directed_input = true;
+                if (i + 1) < len && !is_operator(tokens[i + 1]) {
+                    command.input_filename = Some(tokens[i + 1].to_owned());
+                    i += 1;
+                }
+            } else {
+                command.tokens.push(tokens[i].to_owned());
+            }
+            i += 1;
+        }
+
+        if i != len {
+            if tokens[i] == "|" {
+                command.piped_output = true;
+                pipe_next_input = true;
+                i += 1;
+            }
+        }
+
+        commands.push(command)
     }
 
-    return command;
+    return commands;
 }
 
 fn is_operator(token: &str) -> bool {
-    if token == ">" || token == "<" {
+    if token == ">" || token == "<" || token == "|" {
         return true;
     }
 
     return false;
 }
 
-fn execute_command(command: SingleCommand) {
-    let mut handles = vec![];
+fn execute_commands(commands: Vec<SingleCommand>) {
+    let mut handles: Vec<Child> = vec![];
 
-    let stdout_handle: Stdio = if command.directed_output {
-        let output_file_path = command.output_filename.unwrap();
-        let output_file = File::create(&output_file_path).unwrap();
-        Stdio::from(output_file)
-    } else {
-        Stdio::inherit()
-    };
+    for i in 0..commands.len() {
+        let stdin_handle: Stdio = if commands[i].directed_input {
+            let input_file_path = commands[i].input_filename.as_ref().unwrap();
+            let input_file = File::open(&input_file_path).unwrap();
+            Stdio::from(input_file)
+        } else if commands[i].piped_input {
+            Stdio::from(handles[i - 1].stdout.take().unwrap())
+        } else {
+            Stdio::inherit()
+        };
 
-    let stdin_handle: Stdio = if command.directed_input {
-        let input_file_path = command.input_filename.unwrap();
-        let input_file = File::open(&input_file_path).unwrap();
-        Stdio::from(input_file)
-    } else {
-        Stdio::inherit()
-    };
+        let stdout_handle: Stdio = if commands[i].directed_output {
+            let output_file_path = commands[i].output_filename.as_ref().unwrap();
+            let output_file = File::create(&output_file_path).unwrap();
+            Stdio::from(output_file)
+        } else if commands[i].piped_output {
+            Stdio::piped()
+        } else {
+            Stdio::inherit()
+        };
 
-    let result = Command::new(command.tokens[0].as_str())
-        .args(&command.tokens[1..])
-        .stdin(Stdio::from(stdin_handle))
-        .stdout(Stdio::from(stdout_handle))
-        .spawn();
+        let result = Command::new(commands[i].tokens[0].as_str())
+            .args(&commands[i].tokens[1..])
+            .stdin(Stdio::from(stdin_handle))
+            .stdout(Stdio::from(stdout_handle))
+            .spawn();
 
-    match result {
-        Ok(handle) => {
-            handles.push(handle);
-        }
-        Err(err) => {
-            eprintln!(
-                "Failed to execute '{}': {}",
-                command.tokens[0].as_str(),
-                err
-            );
-        }
-    };
+        match result {
+            Ok(handle) => {
+                handles.push(handle);
+            }
+            Err(err) => {
+                eprintln!(
+                    "Failed to execute '{}': {}",
+                    commands[i].tokens[0].as_str(),
+                    err
+                );
+            }
+        };
+    }
 
     for mut handle in handles {
         if let Err(err) = handle.wait() {
@@ -137,7 +169,7 @@ fn execute_command(command: SingleCommand) {
     }
 }
 
-fn change_dir(command: SingleCommand){
+fn change_dir(command: SingleCommand) {
     if command.tokens.len() != 2 {
         println!("Incorrect amount of arguments for this function");
         return;
